908e9db600022e680b74e49de6e2fb20fcc2a0a9
mazen ramadan <152196951+Mazen024@users.noreply.github.com>
2024-05-03 15:12:03 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGJKBlBpN2CPIMqRSHOA4OXmlZIePZKvQ",
-  authDomain: "chat-9e21d.firebaseapp.com",
-  projectId: "chat-9e21d",
-  storageBucket: "chat-9e21d.appspot.com",
-  messagingSenderId: "335097414750",
-  appId: "1:335097414750:web:0e847936b3f946594d60cf"
+
 };
 
 const app = initializeApp(firebaseConfig);
